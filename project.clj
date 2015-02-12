1bacf5c4e6b4685465751203400b9e3753b79147
Mikhail Dyakov <wtltl2@gmail.com>
2015-02-12 17:28:47 +0100

    Make project.clj prettier.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dict-chrome "0.1.0-SNAPSHOT"
   :description "Smart translate chrome extension."
-  :url "http://example.com/FIXME"
+  :url "https://chrome.google.com/webstore/detail/smart-translate/fmllglnmbaiehbdnnmjppbjjcffnhkcp"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2760"]
@@ -24,16 +24,11 @@
                                         :optimizations :none}}}}
 
   :profiles {:dev {:repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
                    :dependencies [[pjstadig/humane-test-output "0.6.0"]]
-
                    :plugins [[com.cemerick/clojurescript.test "0.3.3"]]
-
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
-
                    :env {:dev? true}
-
                    :cljsbuild {:builds {:app {:compiler {:source-map true}}
                                         :test {:source-paths ["src" "test"]
                                                :compiler {:output-to "test/target/test.js"
@@ -42,9 +37,7 @@
                                :test-commands {"unit" ["slimerjs" :runner
                                                        "test/vendor/console-polyfill.js"
                                                        "test/target/test.js"]}}}
-
-  :production {:env {:production true}
-
+             :production {:env {:production true}
                           :cljsbuild {:builds {:app
                                                {:compiler
                                                 {:optimizations :advanced
